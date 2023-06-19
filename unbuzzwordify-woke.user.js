fee54c9d185944752ca288a3ed49d7f5761c26b9
chrishazfun <lowleschris@gmail.com>
2023-06-20 01:42:45 +1000

    migration to github

---
+++ unbuzzwordify-woke.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Unbuzzwordify (Just Woke)
+// @namespace    https://github.com/chrishazfun
+// @version      1.3.0
+// @description  Replaces specifically the word woke with other, more entertaining words.
+// @source       https://github.com/chrishazfun
+// @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/unbuzzwordify-woke.user.js
+// @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/unbuzzwordify-woke.user.js
+// @author       chrishazfun
+// @icon         https://raw.githubusercontent.com/chrishazfun/userscripts/main/unbuzzwordify.png
+// -- sites that break with this
+// @exclude-match https://onlyfans.com/*
+// @exclude-match https://*pcpartpicker*/*
+// @exclude-match https://webapp.licenceready.com.au/*
+// @exclude-match *://*.deviantart.com/*
+// @exclude-match *://*.bandcamp.com/*
+// ==/UserScript==
+
+function unbuzzwordify() {
+	var elements = document.querySelectorAll('*');
+	var phrases = [
+		"WAH",
+		"GOATSE",
+		"BIG FAT CAWK",
+		"WAP",
+		"AIRPLANES FULL OF SNAKES"
+	];
+	for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+		for (var j = 0; j < element.childNodes.length; j++) {
+			var node = element.childNodes[j];
+			if (node.nodeType === 3) {
+				var txt = node.nodeValue;
+				var replaced = txt.replace(/(woke)/gi, phrases[Math.floor(Math.random()*phrases.length)]);
+				// var replaced = replaced.replace(/woke/gi, `${} `);
+				if (replaced !== txt) element.replaceChild(document.createTextNode(replaced), node);
+			}
+		}
+	}
+}
+
+setTimeout(unbuzzwordify, 1000);
+setInterval(unbuzzwordify, 2000);
---
+++ zero-out-yt-timestamp.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Zero Out YT Timestamp
+// @namespace    https://github.com/chrishazfun
+// @version      1.2
+// @description  Zeros out any youtube.com timestamp upon loading, might be helpful for certain situations.
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @source       https://github.com/chrishazfun
+// @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/zero-out-yt-timestamp.user.js
+// @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/zero-out-yt-timestamp.user.js
+// @author       chrishazfun
+// @match        *youtube.com*
+// @include      *youtube.com*
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.2
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// ==/UserScript==
+
+if (new URL(window.location.href).searchParams.get("t").replace("s", "") > 0) {
+	let url = new URL(window.location.href);
+	let params = new URLSearchParams(url.search);
+	params.set('t', 0);
+	window.location.search = `?${params.toString()}`;
+}
