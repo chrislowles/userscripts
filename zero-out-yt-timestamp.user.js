572f17b70eef4ff672fefc2b79ae0fcc104b5a93
chrishazfun <lowleschris@gmail.com>
2023-06-21 02:32:36 +1000

    @match and @include error fixed

--- zero-out-yt-timestamp.user.js
+++ zero-out-yt-timestamp.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Zero Out YT Timestamp
 // @namespace    https://github.com/chrishazfun
-// @version      1.2.3
+// @version      1.2.4
 // @description  Zeros out any youtube.com timestamp upon loading, might be helpful for certain situations.
 // @source       https://github.com/chrishazfun
 // @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/zero-out-yt-timestamp.user.js
 // @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/zero-out-yt-timestamp.user.js
 // @author       chrishazfun
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @match        *youtube.com*
-// @include      *youtube.com*
+// @match        *://*youtube.com*/*
+// @include      *://*youtube.com*/*
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.2
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
