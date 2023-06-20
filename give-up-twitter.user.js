f318df9e3b9c1b7071b09799e02e6cb99101388f
chrishazfun <lowleschris@gmail.com>
2023-06-21 01:19:28 +1000

    Update give-up-twitter.user.js

--- give-up-twitter.user.js
+++ give-up-twitter.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Give Up Twitter
 // @namespace    https://github.com/chrishazfun
-// @version      1.4.0
+// @version      1.5.0
 // @description  Redirects any twitter.com (subdomains too) url to Rick Astleys smash hit.
 // @source       https://github.com/chrishazfun
 // @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
 // @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
 // @author       chrishazfun
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
-// @match        *twitter.com*
-// @include      *twitter.com*
+// @match        *://*twitter.com*/*
+// @include      *://*twitter.com*/*
 // ==/UserScript==
 
 window.location.href = "https://youtu.be/embed/dQw4w9WgXcQ?autoplay=true";
