1d24916f260cc387ca9a5aa4a2a4629dfe38d8b7
chrishazfun <lowleschris@gmail.com>
2023-08-23 00:18:47 +1000

    added domains for when musk decides to kill the app i mean commit to x.com

--- give-up-twitter.user.js
+++ give-up-twitter.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
-// @name         Give Up Twitter
+// @name         Give Up Twitter/X
 // @namespace    https://github.com/chrishazfun
-// @version      1.5.3
-// @description  Redirects any twitter.com (subdomains too) url to Rick Astleys smash hit.
+// @version      1.5.4
+// @description  Redirects any twitter.com/x.com (subdomains too) url to Rick Astleys smash hit.
 // @source       https://github.com/chrishazfun
 // @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
 // @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
 // @author       chrishazfun
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
-// @match        *://*twitter.com*/*
-// @include      *://*twitter.com*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
+// @match        *://*twitter.com/*
+// @include      *://*twitter.com/*
+// @match        *://*.x.com/*
+// @include      *://xpro.x.com/*
 // ==/UserScript==
 
 window.location.href = "https://youtu.be/dQw4w9WgXcQ";
