acda202752afd5bc58c82f72a09d030616c84ce4
chrishazfun <lowleschris@gmail.com>
2023-08-23 23:42:11 +1000

    font size tweaks

--- ddgl-google-bing.user.js
+++ ddgl-google-bing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name DDG Lite > Google or Bing
 // @namespace https://github.com/chrishazfun
-// @version 1.0.6
+// @version 1.0.7
 // @description Adds two link shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
@@ -16,13 +16,15 @@ let google = document.createElement('a');
 google.href = `https://www.google.com/search?q=${document.querySelector("input.query").value}`;
 google.target = "_top";
 google.innerHTML = "Google";
-google.style.margin = "0 0 0 5px";
+google.style.margin = "0 0 0 10px";
+google.style.fontSize = "1.4rem";
 
 let bing = document.createElement('a');
 bing.href = `https://www.bing.com/search?q=${document.querySelector("input.query").value}`;
 bing.target = "_top";
 bing.innerHTML = "Bing";
-bing.style.margin = "0 0 0 5px";
+bing.style.margin = "0 0 0 10px";
+bing.style.fontSize = "1.4rem";
 
 document.querySelector(".filters select[name='df']").after(google)
 document.querySelector(".filters select[name='df']").after(bing)
