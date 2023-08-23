70b3612726714406ca4fd4abdd8b8ad4cf242ef0
chrishazfun <lowleschris@gmail.com>
2023-08-23 23:05:56 +1000

    spacer added, trying out lets instead of vars

--- ddgl-google-bing.user.js
+++ ddgl-google-bing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DDG Lite > Google or Bing
 // @namespace    https://github.com/chrishazfun
-// @version      1.0.2
+// @version      1.0.3
 // @description  Adds two link shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
 // @source       https://github.com/chrishazfun
 // @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
@@ -11,18 +11,23 @@
 // @match        http*://lite.duckduckgo.com/lite*?q=*
 // ==/UserScript==
 
-var google = document.createElement('a');
+let spacer = document.createElement('div');
+spacer.href = "display:inline;width:5px";
+
+let google = document.createElement('a');
 google.href = `https://www.google.com/search?q=${document.querySelector("input.query").value}`;
 google.target = "_blank";
 google.innerHTML = "Google";
 // document.querySelector(".filters table tbody tr td:nth-of-type(2)").appendChild(google);
 
-var bing = document.createElement('a');
+let bing = document.createElement('a');
 bing.href = `https://www.bing.com/search?q=${document.querySelector("input.query").value}`;
 // bing.style = "display:block";
 bing.target = "_blank";
 bing.innerHTML = "Bing";
 // document.querySelector(".filters table tbody tr td:nth-of-type(2)").appendChild(bing);
 
+document.querySelector(".filters select[name='df']").after(spacer)
 document.querySelector(".filters select[name='df']").after(google)
+document.querySelector(".filters select[name='df']").after(spacer)
 document.querySelector(".filters select[name='df']").after(bing)
