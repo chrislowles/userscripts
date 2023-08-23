8a21b2cc78d7402c4f57aa6daed52c27701c64de
chrishazfun <lowleschris@gmail.com>
2023-08-23 23:24:34 +1000

    [POST] DDGL form doesn't set off script, spacer tweaks, links open in same window

--- ddgl-google-bing.user.js
+++ ddgl-google-bing.user.js
@@ -1,29 +1,29 @@
 // ==UserScript==
-// @name         DDG Lite > Google or Bing
-// @namespace    https://github.com/chrishazfun
-// @version      1.0.4
-// @description  Adds two link shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
-// @source       https://github.com/chrishazfun
-// @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
-// @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
-// @author       chrishazfun
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=duckduckgo.com
-// @match        http*://lite.duckduckgo.com/lite*?q=*
+// @name DDG Lite > Google or Bing
+// @namespace https://github.com/chrishazfun
+// @version 1.0.4
+// @description Adds two link shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
+// @source https://github.com/chrishazfun
+// @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
+// @downloadURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
+// @author chrishazfun
+// @icon https://www.google.com/s2/favicons?sz=64&domain=duckduckgo.com
+// @match http*://lite.duckduckgo.com/lite*?q=*
+// @match https://lite.duckduckgo.com/lite/
 // ==/UserScript==
 
-let spacer = document.createElement('div');
-spacer.href = "display:inline;width:5px";
+let spacer = document.createElement('span');
+spacer.href = "width:5px";
 
 let google = document.createElement('a');
 google.href = `https://www.google.com/search?q=${document.querySelector("input.query").value}`;
-google.target = "_blank";
+google.target = "_top";
 google.innerHTML = "Google";
 // document.querySelector(".filters table tbody tr td:nth-of-type(2)").appendChild(google);
 
 let bing = document.createElement('a');
 bing.href = `https://www.bing.com/search?q=${document.querySelector("input.query").value}`;
-// bing.style = "display:block";
-bing.target = "_blank";
+bing.target = "_top";
 bing.innerHTML = "Bing";
 // document.querySelector(".filters table tbody tr td:nth-of-type(2)").appendChild(bing);
 
