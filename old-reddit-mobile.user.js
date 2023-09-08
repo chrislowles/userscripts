d9b8b50873ca2c7513e785373e05a102c43a8629
chrishazfun <lowleschris@gmail.com>
2023-09-09 08:21:27 +1000

    set offset for styles being set

--- old-reddit-mobile.user.js
+++ old-reddit-mobile.user.js
@@ -12,29 +12,31 @@
 // ==/UserScript==
 
 if (window.location.host == "old.reddit.com") {
-    // viewport is non-standard
-    document.querySelector("meta[name='viewport']").setAttribute("content", "width=device-width, initial-scale=1");
-    // restyle for mobile
-    let styles = `
-    body {
-        display: flex;
-        flex-direction: column;
-    }
-    .side {
-        float: unset;
-        background-color: unset;
-        margin: 0px 5px 0 5px;
-        width: unset;
-    }
-    #header { order: 1; }
-    .side { order: 3; }
-    .content { order: 2; }
-    .footer-parent { order: 4; }
-    .debuginfo { order: 5; }
-    `
-    let stylesEl = document.createElement("style");
-    stylesEl.innerHTML = styles;
-    document.querySelector("head").prepend(stylesEl);
+    setTimeout(() => {
+        // viewport is non-standard
+        document.querySelector("meta[name='viewport']").setAttribute("content", "width=device-width, initial-scale=1");
+        // restyle for mobile
+        let styles = `
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+        .side {
+            float: unset;
+            background-color: unset;
+            margin: 0px 5px 0 5px;
+            width: unset;
+        }
+        #header { order: 1; }
+        .side { order: 3; }
+        .content { order: 2; }
+        .footer-parent { order: 4; }
+        .debuginfo { order: 5; }
+        `
+        let stylesEl = document.createElement("style");
+        stylesEl.innerHTML = styles;
+        document.querySelector("head").prepend(stylesEl);
+    }, 100);
 }
 
 // Used for optimization when there's too many links on a page
