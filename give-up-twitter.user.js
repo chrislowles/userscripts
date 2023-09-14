8bc32aed193bedda90afce8c5c514b01b7d02251
chrishazfun <lowleschris@gmail.com>
2023-09-14 21:48:05 +1000

    whitespace, version bump

--- give-up-twitter.user.js
+++ give-up-twitter.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         Give Up Twitter/X
-// @namespace    https://github.com/chrishazfun
-// @version      1.5.8
-// @description  Redirects any twitter.com/x.com (subdomains too) url to Rick Astleys smash hit.
-// @source       https://github.com/chrishazfun
-// @updateURL    https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
-// @downloadURL  https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
-// @author       chrishazfun
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
-// @match      http*://*.twitter.com/*
-// @match      http*://*.x.com/*
+// @name Give Up Twitter/X
+// @namespace https://github.com/chrishazfun
+// @version 1.5.9
+// @description Redirects any twitter.com/x.com (subdomains too) url to Rick Astleys smash hit.
+// @source https://github.com/chrishazfun
+// @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
+// @downloadURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
+// @author chrishazfun
+// @icon https://www.google.com/s2/favicons?sz=64&domain=x.com
+// @match http*://*.twitter.com/*
+// @match http*://*.x.com/*
 // ==/UserScript==
 
 window.location.href = "https://youtu.be/dQw4w9WgXcQ";
