bf7647524e59a6a0150510124bd97c87282c8d46
chrishazfun <lowleschris@gmail.com>
2023-09-28 18:05:19 +1000

    style adjustments on links to fit with unstyled forms

--- ddgl-google-bing.user.js
+++ ddgl-google-bing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name DDG Lite > Google or Bing
 // @namespace https://github.com/chrishazfun
-// @version 1.0.9
+// @version 1.0.10
 // @description Adds two link shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
@@ -16,15 +16,15 @@ let google = document.createElement('a');
 google.href = `https://www.google.com/search?q=${document.querySelector("input.query").value}`;
 google.target = "_top";
 google.innerHTML = "Google";
-google.style.margin = "0 0 0 10px";
-google.style.fontSize = "1.4rem";
+// google.style.margin = "0 0 0 10px";
+google.style.fontSize = "1.1rem";
 
 let bing = document.createElement('a');
 bing.href = `https://www.bing.com/search?q=${document.querySelector("input.query").value}`;
 bing.target = "_top";
 bing.innerHTML = "Bing";
-bing.style.margin = "0 0 0 13px";
-bing.style.fontSize = "1.4rem";
+// bing.style.margin = "0 0 0 13px";
+bing.style.fontSize = "1.1rem";
 
 document.querySelector(".filters select[name='df']").after(google)
 document.querySelector(".filters select[name='df']").after(bing)
