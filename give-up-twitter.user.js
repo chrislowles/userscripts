80f18fd4bc9602213fa685f4cd8baa26f9b884a5
chrishazfun <lowleschris@gmail.com>
2023-10-30 04:04:20 +1100

    fix for embed catcher in give-up-twitter

--- give-up-twitter.user.js
+++ give-up-twitter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name Give Up Twitter/X
 // @namespace https://github.com/chrishazfun
-// @version 1.6.0
-// @description Redirects any twitter.com/x.com (subdomains too) url to Rick Astleys smash hit. NEW: Twitter embeds may get replaced with a YouTube embed of the song too.
+// @version 1.6.1
+// @description Redirects any twitter.com/x.com (subdomains too) url and embed to Rick Astleys smash hit.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
 // @downloadURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
@@ -12,9 +12,11 @@
 // @match http*://*.x.com/*
 // ==/UserScript==
 
-window.location.href = "https://youtu.be/dQw4w9WgXcQ";
-
 if (!(window === window.parent)) {
-	// quick and dirty means to redirect embeds to the video too lol
-	window.location.href = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+	// iframes get it too
+	setTimeout(() => {
+		window.location.href = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+	}, 500)
+} else {
+	window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
