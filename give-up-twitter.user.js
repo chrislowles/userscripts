90727574d0b5edfa188e30cdf0eb1a90bdb7853e
chrishazfun <lowleschris@gmail.com>
2023-10-29 17:16:47 +1100

    give-up-twitter: turn embeds into youtube embeds of the song

--- give-up-twitter.user.js
+++ give-up-twitter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name Give Up Twitter/X
 // @namespace https://github.com/chrishazfun
-// @version 1.5.9
-// @description Redirects any twitter.com/x.com (subdomains too) url to Rick Astleys smash hit.
+// @version 1.6.0
+// @description Redirects any twitter.com/x.com (subdomains too) url to Rick Astleys smash hit. NEW: Twitter embeds may get replaced with a YouTube embed of the song too.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
 // @downloadURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/give-up-twitter.user.js
@@ -13,3 +13,8 @@
 // ==/UserScript==
 
 window.location.href = "https://youtu.be/dQw4w9WgXcQ";
+
+if (!(window === window.parent)) {
+	// quick and dirty means to redirect embeds to the video too lol
+	window.location.href = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+}
