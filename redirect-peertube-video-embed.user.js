6a1006fa100681f57e112d277382f258f9cf6379
chrishazfun <lowleschris@gmail.com>
2023-11-22 17:04:37 +1100

    redirect-peertube-video-embed: change tab title to use video title instead of instance title

--- redirect-peertube-video-embed.user.js
+++ redirect-peertube-video-embed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Redirect PeerTube Video to Fullscreen Embed
 // @namespace https://github.com/chrishazfun
-// @version 1.0.0
+// @version 1.0.1
 // @description Redirects PeerTube videos to a fullscreen embed url that autoplays, QoL userscript for people that sub to PeerTube channels through Mastodon or other ActivityPub-supported platforms.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/redirect-peertube-video-embed.user.js
@@ -16,4 +16,14 @@ if (
   document.querySelectorAll(".standalone-video-embed").length < 1
 ) {
   window.location.href = document.querySelectorAll("meta[property='og:video:secure_url']")[0].getAttribute("content");
+}
+
+
+// set title on fullscreen-ish embed page redirect
+if (
+  document.querySelectorAll("meta[property='og:platform']").length > 0 &&
+  document.querySelectorAll("meta[property='og:platform']")[0].getAttribute("content") == "PeerTube" &&
+  document.querySelectorAll(".standalone-video-embed").length == 1
+) {
+  document.querySelectorAll("title")[0].innerHTML = document.querySelectorAll(".peertube-dock-title")[0].innerHTML
 }
