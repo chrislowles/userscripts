3ed1e8823be9d35315192c2d18f07c0e8a9d4c9a
chrishazfun <lowleschris@gmail.com>
2023-12-06 03:50:54 +1100

    phanpy-tweaks: undone rework

--- phanpy-tweaks.user.js
+++ phanpy-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Tweaks for Phanpy.social
 // @namespace https://github.com/chrishazfun
-// @version 1.0.3
+// @version 1.0.4
 // @description Small tweaks for Phanpy.social, a minimal Mastodon client.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/phanpy-tweaks.user.js
@@ -12,19 +12,20 @@
 // ==/UserScript==
 
 setTimeout(function() {
-  let phanpyStyles = `
-<style>
+  let phanpyStyles = document.createElement("style");
+  phanpyStyles.innerHTML = `
 *, * * {
-  animation: unset;
-  transition: unset;
+  animation: unset !important;
+  transition: unset !important;
 }
+
 #compose-container .status-preview {
   max-height: unset;
 }
+
 #generic-accounts-container .accounts-list li {
-  flex-basis: 100%;
+    flex-basis: 100% !important;
 }
-</style>
-`;
-  document.querySelector('head').insertAdjacentHTML('beforeend', phanpyStyles);
+  `;
+  document.querySelector("head").prepend(phanpyStyles);
 }, 1);
