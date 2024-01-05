31ff2c68883337920e82e5555ac4c1e56a3467e5
chrishazfun <lowleschris@gmail.com>
2024-01-06 04:25:56 +1100

    ap-open-remote-profile: great it's broken again, fixes

--- ap-open-remote-profile.user.js
+++ ap-open-remote-profile.user.js
@@ -1,74 +1,74 @@
 // ==UserScript==
 // @name         ActivtyPub - Open Remote Profile in Home Instance
 // @namespace    https://mastodon.social/
-// @version      0.1
+// @version      0.2
 // @description  Simple script to add button that opens remote profile in a home instance, generic just in case.
 // @author       chrishazfun
 // @match        https://*/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+setTimeout(function() {
+    (function() {
+        'use strict';
 
-    // Waits for a node to exist before returning it
-    function getNode(selector) {
-        return new Promise(resolve => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-            }
-
-            const observer = new MutationObserver(mutations => {
+        // Waits for a node to exist before returning it
+        function getNode(selector) {
+            return new Promise(resolve => {
                 if (document.querySelector(selector)) {
                     resolve(document.querySelector(selector));
-                    observer.disconnect();
+                }
+                const observer = new MutationObserver(mutations => {
+                    if (document.querySelector(selector)) {
+                        resolve(document.querySelector(selector));
+                        observer.disconnect();
+                    }
+                });
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true
+                });
+            });
+        }
+
+        async function createLink() {
+            if (!document.querySelector("#remote-follow")) {
+                const accountBtns = document.querySelectorAll(".account__header__tabs__buttons")[0];
+                const newBtn = document.createElement("a");
+                newBtn.href = `https://${myServer}/${document.querySelector(".account__header__tabs__name h1 small").innerText}`;
+                newBtn.innerText = "Open in Home Instance";
+                newBtn.style.appearance = "button";
+                newBtn.classList.add("button");
+                accountBtns.prepend(newBtn);
+            }
+        }
+
+        function detectPageChange() {
+            let prevURL = '';
+            const observer = new MutationObserver((mutations) => {
+                if (location.href !== prevURL) {
+                    prevURL = window.location.href;
+                    if (host !== myServer && prevURL.match(/^https:\/\/.*\/@.*$/)) {
+                        createLink();
+                    }
                 }
             });
 
-            observer.observe(document.body, {
+            observer.observe(document, {
                 childList: true,
                 subtree: true
             });
-        });
-    }
-
-    async function createLink() {
-        if (!document.querySelector("#remote-follow")) {
-            const accountBtns = document.querySelectorAll(".account__header__tabs__buttons")[0];
-            const newBtn = document.createElement("a");
-            newBtn.href = `https://${myServer}/${document.querySelector(".account__header__tabs__name h1 small").innerText}`;
-            newBtn.innerText = "Open in Home Instance";
-            newBtn.style.appearance = "button";
-            newBtn.classList.add("button");
-            accountBtns.prepend(newBtn);
         }
-    }
 
-    function detectPageChange() {
-        let prevURL = '';
-        const observer = new MutationObserver((mutations) => {
-            if (location.href !== prevURL) {
-                prevURL = window.location.href;
-                if (host !== myServer && prevURL.match(/^https:\/\/.*\/@.*$/)) {
-                    createLink();
-                }
-            }
-        });
+        const myServer = "mastodon.social";
+        const host = window.location.host;
+        const url = window.location.href;
+        const body = document.querySelector("body > #mastodon");
 
-        observer.observe(document, {
-            childList: true,
-            subtree: true
-        });
-    }
-
-    const myServer = "mastodon.social";
-    const host = window.location.host;
-    const url = window.location.href;
-    const body = document.querySelector("body > #mastodon");
-
-    if (host !== myServer && url.match(/^https:\/\/.*\/@.*$/) && body) {
-        createLink();
-    } else if (body) {
-        detectPageChange();
-    }
-})();
+        if (host !== myServer && url.match(/^https:\/\/.*\/@.*$/) && body) {
+            createLink();
+        } else if (body) {
+            detectPageChange();
+        }
+    })();
+}, 100);
