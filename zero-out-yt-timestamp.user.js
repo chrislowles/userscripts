cbd74a264c8b5c18aace9a6992e9c88fc16bdc46
chrishazfun <lowleschris@gmail.com>
2024-04-23 13:46:37 +1000

    reworking zero out timestamps to work on events

--- zero-out-yt-timestamp.user.js
+++ zero-out-yt-timestamp.user.js
@@ -11,9 +11,24 @@
 // @include http*://www.youtube.com/*
 // ==/UserScript==
 
-if (new URL(window.location.href).searchParams.get("t").replace("s", "") > 0) {
-	let url = new URL(window.location.href);
-	let params = new URLSearchParams(url.search);
-	params.set('t', 0);
-	window.location.search = `?${params.toString()}`;
+document.addEventListener("yt-navigate-start", function() {
+	callToThing();
+});
+  
+document.addEventListener("yt-navigate-finish", function(event) {
+	console.log(event.detail.pageType, event);
+	if (event.detail.pageType == "watch") callToThing();
+});
+
+function callToThing() {
+	if (new URL(window.location.href).searchParams.get("t").replace("s", "") > 0) {
+		if (confirm("We detected you have a timestamp in this video, do you want to clear it out?") == true) {
+			let url = new URL(window.location.href);
+			let params = new URLSearchParams(url.search);
+			params.set('t', 0);
+			window.location.search = `?${params.toString()}`;
+		} else {
+			return false;
+		}
+	}
 }
