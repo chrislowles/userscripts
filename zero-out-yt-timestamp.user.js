8309320c6a1a94c3a188d2749e28106847b47c44
chrishazfun <lowleschris@gmail.com>
2024-04-30 01:40:17 +1000

    zero-out-yt-timestamp: events moving again

--- zero-out-yt-timestamp.user.js
+++ zero-out-yt-timestamp.user.js
@@ -11,23 +11,21 @@
 // @include http*://www.youtube.com/*
 // ==/UserScript==
 
-function callToThing() {
-	if (new URL(window.location.href).searchParams.get("t").replace("s", "") > 0) {
-		if (confirm("We detected you have a timestamp in this video, do you want to clear it out and start from the beginning?") == true) {
-			let url = new URL(window.location.href);
-			let params = new URLSearchParams(url.search);
-			params.set('t', 0);
-			window.location.search = `?${params.toString()}`;
-		} else {
-			return false;
-		}
-	}
-}
-
 //document.addEventListener("yt-navigate-start", function(event) {
 //});
 
 document.addEventListener("yt-navigate-finish", function(event) {
 	console.log(event.detail.pageType, event);
-	if (event.detail.pageType == "watch") callToThing();
+	if (event.detail.pageType == "watch") {
+		if (new URL(window.location.href).searchParams.get("t").replace("s", "") > 0) {
+			if (confirm("We detected you have a timestamp in this video, do you want to clear it out and start from the beginning?") == true) {
+				let url = new URL(window.location.href);
+				let params = new URLSearchParams(url.search);
+				params.set('t', 0);
+				window.location.search = `?${params.toString()}`;
+			} else {
+				return false;
+			}
+		}
+	}
 });
