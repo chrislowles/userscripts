ddf98f1f5fe663ec44673679e688bd07b6159c39
chrishazfun <lowleschris@gmail.com>
2024-05-19 16:27:34 +1000

    Update ddgl-google-bing.user.js

--- ddgl-google-bing.user.js
+++ ddgl-google-bing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name DDG Lite > Google or Bing
 // @namespace https://github.com/chrishazfun
-// @version 1.0.12
+// @version 1.0.14
 // @description Adds two link shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
 // @source https://github.com/chrishazfun
 // @updateURL https://raw.githubusercontent.com/chrishazfun/userscripts/main/ddgl-google-bing.user.js
@@ -12,19 +12,24 @@
 // @match https://lite.duckduckgo.com/lite/
 // ==/UserScript==
 
+let engines = document.createElement("div");
+engines.className = "engines";
+engines.style.display = "inline";
+document.querySelector(".filters select[name='df']").after(engines);
+
 let google = document.createElement('a');
 google.href = `https://www.google.com/search?q=${document.querySelector("input.query").value}`;
 google.target = "_top";
 google.innerHTML = "Google";
-// google.style.margin = "0 0 0 10px";
+google.style.margin = "0 0 0 .5rem";
 google.style.fontSize = "1.1rem";
 
 let bing = document.createElement('a');
 bing.href = `https://www.bing.com/search?q=${document.querySelector("input.query").value}`;
 bing.target = "_top";
 bing.innerHTML = "Bing";
-// bing.style.margin = "0 0 0 13px";
+bing.style.margin = "0 0 0 .8rem";
 bing.style.fontSize = "1.1rem";
 
-document.querySelector(".filters select[name='df']").after(google)
-document.querySelector(".filters select[name='df']").after(bing)
+document.querySelector(".engines").insertAdjacentHTML('beforeend', google);
+document.querySelector(".engines").insertAdjacentHTML('beforeend', bing);
