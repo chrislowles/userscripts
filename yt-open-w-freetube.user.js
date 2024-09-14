8a57fd4e54735e9bece54b3910d820297663ad73
chrishazfun <lowleschris@gmail.com>
2024-09-14 18:35:00 +1000

    yt-open-w-freetube update
    youtube moves elements around too much so i think i'm just gonna stick with prompts and escapes, might end up doing the same stuff for the other scripts that deal with element detection and kill the auto versions of my scripts

--- yt-open-w-freetube.user.js
+++ yt-open-w-freetube.user.js
@@ -2,7 +2,7 @@
 // @name YouTube: Open w/ FreeTube
 // @description Adds a button to open the currently opened video in FreeTube (if installed)
 // @author Chris Lowles
-// @version 2024.9.14
+// @version 2024.9.14-1
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-w-freetube.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-w-freetube.user.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
@@ -10,34 +10,13 @@
 // @run-at document-start
 // ==/UserScript==
 
-document.addEventListener("yt-navigate-start", function () {
-  document.querySelector("openwft").remove();
-});
-
 document.addEventListener("yt-navigate-finish", function (event) {
   console.log(event.detail.pageType, event);
   if (event.detail.pageType == "watch") {
-    waitForKeyElements("#bottom-row #owner", () => {
-      document.querySelector("#bottom-row #owner #subscribe-button").insertAdjacentHTML("afterend", `
-        <openwft>
-          <style>
-            .open-freetube {
-              padding: 10px 12px;
-              margin-left: 8px;
-              border-radius: 17px;
-              font-size: 13.5px;
-              font-weight: bold;
-              color: var(--primary-background-color);
-              background: var(--paper-menu-background-color);
-              text-decoration: none;
-            } .open-freetube:hover {
-              text-decoration:underline;
-            }
-          </style>
-          <a href="freetube://${window.location.href}" class="open-freetube">FreeTube</a>
-        </openwft>
-      `);
-    });
-    waitForEl();
+    if (confirm("Open in FreeTube?") == true) {
+      window.open(`freetube://${window.location.href}`, "_top");
+    } else {
+      return false;
+    }
   }
 });
