3180a4e618314a3d404934ac6c80e25572df1e25
Chris Lowles <lowleschris@gmail.com>
2024-12-23 00:10:39 +1100

    Update yt-open-w-freetube.user.js

--- yt-open-w-freetube.user.js
+++ yt-open-w-freetube.user.js
@@ -2,7 +2,7 @@
 // @name YouTube: Open w/ FreeTube
 // @description Adds a button to open the currently opened video in FreeTube (if installed)
 // @author Chris Lowles
-// @version 2024.9.14-1
+// @version 2024.12.23
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-w-freetube.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-w-freetube.user.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
