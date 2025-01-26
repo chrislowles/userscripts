5d3471ba202a40c27fe73dd032157e006b585bf8
Chris Lowles <lowleschris@gmail.com>
2025-01-27 06:44:53 +1100

    Update ddgl-google-bing.user.js

--- ddgl-google-bing.user.js
+++ ddgl-google-bing.user.js
@@ -2,11 +2,14 @@
 // @name DDG Lite > Google or Bing
 // @description Adds two shortcuts to open any query made on DuckDuckGo Lite on either Google or Bing, the ebil search engines.
 // @author Chris Lowles
-// @version 2024.12.23
+// @version 2025.1.15
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/ddgl-google-bing.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/ddgl-google-bing.user.js
 // @match http*://lite.duckduckgo.com/lite*?q=*
 // ==/UserScript==
+
+var google = document.createElement("a");
+google.setAttribute("href", `https://www.google.com/search?q=${document.querySelector("input.query").value}`);
 
 document.querySelector(".filters select[name='df']").after(`
     <a href="https://www.google.com/search?q=${document.querySelector("input.query").value}" style="margin:0 0 0 .5rem;font-size:1.1rem;">Google</a>
