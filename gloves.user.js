55ee33e79b1dda9926ee8c245c4e4ce74a6b05e9
Chris Lowles <lowleschris@gmail.com>
2025-02-17 17:02:16 +1100

    Update gloves.user.js

--- gloves.user.js
+++ gloves.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
+// @run-at document-start
 // @name Gloves
 // @description Gloves for when you're made to read or interact with oligarch-run media.
 // @author Chris Lowles
-// @version 2025.2.4
-// @run-at document-start
+// @version 2025.2.17
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
