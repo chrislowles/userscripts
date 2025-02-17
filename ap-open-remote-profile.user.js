8b2d5f41b311ba1607b350da7d3f5f315331a351
Chris Lowles <lowleschris@gmail.com>
2025-02-17 17:01:52 +1100

    Update ap-open-remote-profile.user.js

--- ap-open-remote-profile.user.js
+++ ap-open-remote-profile.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
+// @run-at document-start
 // @name ActivityPub - Open Remote Profile in Home Instance
 // @description Adds button that opens remote profile in a home instance, generic just in case.
 // @author Chris Lowles
-// @version 2024.12.23
+// @version 2025.2.17
 // @match *://*/*
 // ==/UserScript==
