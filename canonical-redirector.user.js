99f9487afb0a33224466524d87e4fb610e6dafd5
Chris Lowles <lowleschris@gmail.com>
2025-04-05 16:29:56 +1100

    Create canonical-redirector.user.js

---
+++ canonical-redirector.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @run-at document-start
+// @name Canonical Redirector
+// @license MIT
+// @description Automatically redirect news links to the original source site, based on MSN Redirector by Jamie Landeg-Jones
+// @author Jamie Landeg-Jones, Chris Lowles
+// @version 2025.4.5
+// @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/canonical-redirector.user.js
+// @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/canonical-redirector.user.js
+// @grant none
+// -- SUPPORTED SITES --
+// @match https://www.yahoo.com/*
+// @match https://www.msn.com/*
+// ==/UserScript==
+
+// Props to the hints from this post: joeytwiddle@github - https://github.com/Tampermonkey/tampermonkey/issues/1279#issuecomment-875386821
+
+(function() {
+	'use strict';
+	function redirect_to_article() {
+		let numAttempts = 0;
+		let doneit = 0;
+		let tryNow = function() {
+			let link_tags = document.getElementsByTagName('link');
+			for (let loop=0; loop < link_tags.length; loop++) {
+				if (link_tags[loop].rel && link_tags[loop].rel == 'canonical') {
+					const new_url = link_tags[loop].href;
+					doneit = 1;
+					if (new_url.match("^https:\/\/(www\.)?yahoo.com\/") || new_url.match("^https:\/\/(www\.)?msn.com\/")) {
+						console.info(`Canonical Redirect: Ignoring redirect (same site): ${new_url}`);
+					} else {
+						console.info(`Canonical Redirect: Redirecting to ${new_url}`);
+						window.location = new_url
+					}
+				}
+			}
+			if (!doneit) {
+				if (numAttempts++ >= 20) {
+					console.warn('Giving up after 20 attempts. Could not find canonical link');
+				} else {
+					console.info(`Canonical Redirect: Retrying, attempt ${numAttempts.toString()} of 20.`);
+					setTimeout(tryNow, 250 * Math.pow (1.1, numAttempts));
+				}
+			}
+		}
+		tryNow();
+	}
+	// Run the filter when the page loads
+	window.addEventListener('load', redirect_to_article);
+})();
