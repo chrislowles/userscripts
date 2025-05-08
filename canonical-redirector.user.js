96d24e6cdd896d172f8589d10ba5c967a113e036
Chris Lowles <lowleschris@gmail.com>
2025-05-08 16:38:43 +1000

    Update canonical-redirector.user.js

--- canonical-redirector.user.js
+++ canonical-redirector.user.js
@@ -4,7 +4,7 @@
 // @license MIT
 // @description Automatically redirect referral articles to the original source, based on MSN Redirector by Jamie Landeg-Jones.
 // @author Chris Lowles, Jamie Landeg-Jones
-// @version 2025.4.5-4
+// @version 2025.5.8
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/canonical-redirector.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/canonical-redirector.user.js
 // @grant none
@@ -24,13 +24,19 @@
 			let link_tags = document.getElementsByTagName('link');
 			for (let loop = 0; loop < link_tags.length; loop++) {
 				if (link_tags[loop].rel && link_tags[loop].rel == 'canonical') {
-					const new_url = link_tags[loop].href;
+					let new_url = link_tags[loop].href;
 					doneit = 1;
+					if (document.querySelectorAll("script[type='application/ld+json']").length > 0) {
+						console.info(`Canonical Redirect: Checking for referral links`);
+						let ld_json = JSON.parse(document.querySelectorAll("script[type='application/ld+json']")[0])
+						let ld_json_url_domain = new URL(ld_json.provider.url).href
+						// if (ld_json)
+					}
 					if (
-						new_url.match("^https:\/\/(www\.)?yahoo.com\/") ||
+						new_url.match("^https:\/\/(www\.|finance\.)?yahoo.com\/") ||
 						new_url.match("^https:\/\/(www\.)?msn.com\/")
 					) {
-						console.info(`Canonical Redirect: Ignoring redirect (same site): ${new_url}`);
+						console.info(`Canonical Redirect (same site): ${new_url}`);
 					} else {
 						console.info(`Canonical Redirect: Redirecting to ${new_url}`);
 						window.location = new_url;
