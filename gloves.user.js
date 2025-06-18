93faf4cbd789da806d0f4e09c878e2c5c10c532e
Chris Lowles <lowleschris@gmail.com>
2025-06-19 00:52:01 +1000

    gloves: version bump, indentation fix

--- gloves.user.js
+++ gloves.user.js
@@ -3,7 +3,7 @@
 // @name Gloves
 // @description Gloves for when you're made to read or interact with oligarch-run media.
 // @author Chris Lowles
-// @version 2025.4.5
+// @version 2025.6.19
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
@@ -16,9 +16,9 @@
 // grab txt file of outlet domains
 var domains = GM_getResourceText("domains");
 
-// split per new line and filters empty items
+// split per new line and filter empty items
 var domainsSplit = domains.split(/\n/).filter(entry => {
-  return entry.trim() != '';
+	return entry.trim() != '';
 });
 
 // loop through array and push domains with "www." at the front
@@ -26,11 +26,11 @@ domainsSplit.forEach(corp => domainsSplit.push(`www.${corp}`));
 
 // loop through array and redirect if you're on one of their outlets.
 domainsSplit.forEach((domain) => {
-  if (window.location.host == domain) {
-    if (confirm("Wanna see an archive?") == true) {
-      location.href = 'http://web.archive.org/web/*/' + location.href;
-    }
-  }
+	if (window.location.host == domain) {
+		if (confirm("Wanna see an archive?") == true) {
+			location.href = 'http://web.archive.org/web/*/' + location.href;
+		}
+	}
 });
 
 // TODO: SEARCH ENGINE RESULT HIGHLIGHTING BASED ON LIST
