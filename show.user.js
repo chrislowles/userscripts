a47a2b941cfdcc04a8687d5498bdaf37f3c0f0aa
Chris Lowles <lowleschris@gmail.com>
2025-08-07 01:11:53 +1000

    Update show.user.js

--- show.user.js
+++ show.user.js
@@ -8,53 +8,57 @@
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/show.user.js
 // ==/UserScript==
 
-let prevURL = '';
-new MutationObserver((mutations) => {
-  if (location.href !== prevURL) {
-    prevURL = location.href;
-    switch (window.location.host) {
-      case "www.reddit.com":
-      case "reddit.com":
-        if (
-          ![
-            '/gallery',
-            '/media',
-            '/poll',
-            '/notifications'
-          ].find(pathPrefix => window.location.pathname.toLowerCase().startsWith(pathPrefix))
-        ) {
-          window.location.host = 'old.reddit.com';
-        }
-        if (new URL(window.location.href).searchParams.get("tl")) {
-          let url = new URL(window.location.href);
-          let params = new URLSearchParams(url.search);
-          params.delete('tl');
-          window.location.search = `?${params.toString()}`;
-        }
-      break;
-      case "www.youtube.com":
-        if (
-          window.location.pathname == "/watch" &&
-          new URL(window.location.href).searchParams.get("list") !== null &&
-          new URL(window.location.href).searchParams.get("index") !== null
-        ) {
-          let url = new URL(window.location.href);
-          let params = new URLSearchParams(url.search);
-          params.set('list', null);
-          params.set('index', null);
-          params.set('t', 0);
-          window.location.search = `?${params.toString()}`;
-        }
-      break;
-      case "x.com":
-        window.location.hostname = "xcancel.com";
-      break;
-      case "old.reddit.com":
-        document.querySelector("link[rel='stylesheet'][ref='applied_subreddit_stylesheet']").remove()
-      break;
-    }
-  }
+let prevURL = "";
+new MutationObserver(mutations => {
+	if (location.href !== prevURL) {
+		prevURL = location.href;
+		switch (window.location.host) {
+			case "www.reddit.com":
+			case "reddit.com":
+				if (
+					![
+						"/gallery",
+						"/media",
+						"/poll",
+						"/notifications"
+					].find(
+						pathPrefix => window.location.pathname.toLowerCase().startsWith(pathPrefix)
+					)
+				) {
+					window.location.host = "old.reddit.com";
+				}
+				if (
+					new URL(window.location.href).searchParams.get("tl")
+				) {
+					let url = new URL(window.location.href);
+					let params = new URLSearchParams(url.search);
+					params.delete("tl");
+					window.location.search = `?${params.toString()}`;
+				}
+			break;
+			case "www.youtube.com":
+				if (
+					window.location.pathname == "/watch" &&
+					new URL(window.location.href).searchParams.get("list") !== null &&
+					new URL(window.location.href).searchParams.get("index") !== null
+				) {
+					let url = new URL(window.location.href);
+					let params = new URLSearchParams(url.search);
+					params.set("list", null);
+					params.set("index", null);
+					params.set("t", 0);
+					window.location.search = `?${params.toString()}`;
+				}
+			break;
+			case "x.com":
+				window.location.hostname = "xcancel.com";
+			break;
+			case "old.reddit.com":
+				document.querySelector("link[rel='stylesheet'][ref='applied_subreddit_stylesheet']").remove();
+			break;
+		}
+	}
 }).observe(document, {
-  subtree: true,
-  childList: true
+	subtree: true,
+	childList: true,
 });
