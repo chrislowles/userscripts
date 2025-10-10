cfdb6b309ccbf9c6f7c54d57baa75c6bc992c84c
Chris Lowles <lowleschris@gmail.com>
2025-10-10 23:37:10 +1100

    Update yt-open-w-freetube.user.js

--- yt-open-w-freetube.user.js
+++ yt-open-w-freetube.user.js
@@ -3,7 +3,7 @@
 // @name YouTube: Open w/ FreeTube
 // @description Redirects any YT watch pages to be opened in FreeTube/FreeTube Android (if installed)
 // @author Chris Lowles
-// @version 2025.9.30
+// @version 2025.10.10
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-w-freetube.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-w-freetube.user.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
