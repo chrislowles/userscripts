c0da5c782068c8f11150f52bad2ac8bb003b9221
Chris Lowles <lowleschris@gmail.com>
2025-10-10 16:24:26 +1100

    Update unbuzzwordify.user.js

--- unbuzzwordify.user.js
+++ unbuzzwordify.user.js
@@ -3,7 +3,7 @@
 // @name Unbuzzwordify
 // @description Replaces buzzwords with other, more entertaining words.
 // @author Chris Lowles
-// @version 2025.9.20
+// @version 2025.10.10
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/unbuzzwordify.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/unbuzzwordify.user.js
 // -- sites that break with this
