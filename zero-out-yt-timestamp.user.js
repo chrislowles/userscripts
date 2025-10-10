5162757d5a6a7a3cf067f18b684a0541b05a992c
Chris Lowles <lowleschris@gmail.com>
2025-10-10 16:24:33 +1100

    Update zero-out-yt-timestamp.user.js

--- zero-out-yt-timestamp.user.js
+++ zero-out-yt-timestamp.user.js
@@ -3,7 +3,7 @@
 // @name Zero Out YT Timestamp
 // @description Prompts to zero out any youtube.com timestamp upon load, might be helpful for certain situations.
 // @author Chris Lowles
-// @version 2025.9.20-1
+// @version 2025.10.10
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/zero-out-yt-timestamp.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/zero-out-yt-timestamp.user.js
 // @match http*://www.youtube.com/*
