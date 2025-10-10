65107b17dcc54fc5a277aee3c5b5b55b01589a78
Chris Lowles <lowleschris@gmail.com>
2025-10-10 16:24:28 +1100

    Update yt-front-to-subs.user.js

--- yt-front-to-subs.user.js
+++ yt-front-to-subs.user.js
@@ -3,7 +3,7 @@
 // @name YouTube: Front Page > Subfeed
 // @description Redirects the YouTube Front Page to the Subfeed, by adjusting links in elements that direct there as well as simple checking for the path upon load.
 // @author Chris Lowles
-// @version 2025.9.20
+// @version 2025.9.30
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-front-to-subs.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-front-to-subs.user.js
 // @match http*://www.youtube.com/*
