841f74740d92905c849ad46dbcb0d022c1e697ef
Chris Lowles <lowleschris@gmail.com>
2025-10-10 16:24:31 +1100

    Update yt-sub-w-rss-parrot.user.js

--- yt-sub-w-rss-parrot.user.js
+++ yt-sub-w-rss-parrot.user.js
@@ -3,7 +3,7 @@
 // @name YouTube: Subscribe w/ RSS Parrot (WIP)
 // @description Shortcut to ask RSS Parrot to create an RSS bot for YouTube channel that you can then follow, can be used as alternative for subscribing, pairs well with yt-open-w-freetube.
 // @author Chris Lowles
-// @version 2025.9.20
+// @version 2025.9.30
 // @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-sub-w-rss-parrot.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-sub-w-rss-parrot.user.js
 // @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
