1f57a96a7c44cedf92faf5c9600cef0677380ae5
Chris Lowles <lowleschris@gmail.com>
2026-03-18 17:22:02 +1100

    Update give-up-x.user.js

--- give-up-x.user.js
+++ give-up-x.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @run-at document-start
-// @name Give Up X
+// @run-at 		document-start
+// @name 		Give Up X
 // @description Redirects any x.com (subdomains too) url and embed to Rick Astleys smash hit.
-// @author Chris Lowles
-// @version 2025.10.10
-// @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/give-up-x.user.js
+// @author 		Chris Lowles
+// @version 	2026.3.18
+// @updateURL 	https://raw.githubusercontent.com/chrislowles/userscripts/main/give-up-x.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/give-up-x.user.js
-// @match http*://*.x.com/*
-// @match http*://*.twitter.com/*
-// @match http*://pbs.twimg.com/*
+// @match 		http*://*.x.com/*
+// @match 		http*://*.twitter.com/*
+// @match 		http*://pbs.twimg.com/*
 // ==/UserScript==
 
 // if (!(window === window.parent))
