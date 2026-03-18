7a931313844fea0c7c47caf6aab585c55d25ec35
Chris Lowles <lowleschris@gmail.com>
2026-03-18 17:22:04 +1100

    Update gloves.user.js

--- gloves.user.js
+++ gloves.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @run-at document-start
-// @name Gloves
+// @run-at 		document-start
+// @name 		Gloves
 // @description Gloves for when you're made to read or interact with oligarch-run media.
-// @author Chris Lowles
-// @version 2025.10.10
-// @updateURL https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
+// @author 		Chris Lowles
+// @version 	2026.3.18
+// @updateURL 	https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/gloves.user.js
-// @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
-// @resource domains https://raw.githubusercontent.com/chrislowles/userscripts/refs/heads/main/gloves.txt
-// @match *://*/*
-// @grant GM_addStyle
-// @grant GM_getResourceText
+// @require 	https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
+// @resource 	domains https://raw.githubusercontent.com/chrislowles/userscripts/refs/heads/main/gloves.txt
+// @match 		*://*/*
+// @grant 		GM_addStyle
+// @grant 		GM_getResourceText
 // ==/UserScript==
 
 // grab txt file of outlet domains
