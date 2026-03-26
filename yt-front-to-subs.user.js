78d70a4356b95d0753ea8617c4eba1f6452c07bf
Chris Lowles <lowleschris@gmail.com>
2026-03-26 17:58:33 +1100

    Update yt-front-to-subs.user.js

--- yt-front-to-subs.user.js
+++ yt-front-to-subs.user.js
@@ -1,28 +1,27 @@
 // ==UserScript==
-// @run-at 		document-start
-// @name 		YouTube: Front Page > Subfeed
+// @run-at      document-start
+// @name        YouTube: Front Page > Subfeed
 // @description Redirects the YouTube Front Page to the Subfeed, by adjusting links in elements that direct there as well as simple checking for the path upon load.
-// @author 		Chris Lowles
-// @version 	2026.3.18
-// @updateURL 	https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-front-to-subs.user.js
+// @author      Chris Lowles
+// @version     2026.3.26
+// @updateURL   https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-front-to-subs.user.js
 // @downloadURL https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-front-to-subs.user.js
-// @match 		http*://www.youtube.com/*
-// @match 		http*://m.youtube.com/*
+// @match       http*://www.youtube.com/*
+// @match       http*://m.youtube.com/*
 // ==/UserScript==
 
 let prevURL = '';
 new MutationObserver(mutations => {
-	if (location.href !== prevURL) {
-		prevURL = location.href;
-		if (location.pathname == "/") location.pathname = '/feed/subscriptions';
-	}
+    if (location.href !== prevURL) {
+        prevURL = location.href;
+        if (location.pathname == "/") location.pathname = '/feed/subscriptions';
+    }
 }).observe(document, {
-	subtree: true,
-	childList: true
+    subtree: true,
+    childList: true
 });
 
 window.addEventListener('click', e => {
-	const a = e.target.closest('a');
-	const url = new URL(a.href);
-	if (a && url.pathname === '/') a.pathname = '/feed/subscriptions';
+    const a = e.target.closest('a');
+    if (a && new URL(a.href).pathname === '/') a.pathname = '/feed/subscriptions';
 }, true);
