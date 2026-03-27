395c6edee44b3da1e831534964f8385bdc7eef95
Chris Lowles <lowleschris@gmail.com>
2026-03-27 17:04:25 +1100

    Update zero-out-yt-timestamp.user.js

--- zero-out-yt-timestamp.user.js
+++ zero-out-yt-timestamp.user.js
@@ -3,11 +3,13 @@
 // @name         Zero Out YT Timestamp
 // @description  Silently zeros out any youtube.com timestamp on load, forcing playback from the start while cleanly removing the parameter.
 // @author       Chris Lowles
-// @version      2026.3.27
+// @version      2026.3.27-1
 // @updateURL    https://raw.githubusercontent.com/chrislowles/userscripts/main/zero-out-yt-timestamp.user.js
 // @downloadURL  https://raw.githubusercontent.com/chrislowles/userscripts/main/zero-out-yt-timestamp.user.js
 // @match        http*://www.youtube.com/*
 // ==/UserScript==
+
+// Regarding the logic behind this script, it detects the timestamp and adjusts it to 0 seconds which YT interprets as both playing the video from the start while also removing the parameter, it was something I found as a way to force a reset on YT video playback.
 
 let prevURL = '';
 
