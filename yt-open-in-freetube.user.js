d9b0e5ad768fb05bc89fddbd5d94493fd81f7040
Chris Lowles <lowleschris@gmail.com>
2026-05-18 16:59:05 +1000

    Create yt-open-in-freetube.user.js

---
+++ yt-open-in-freetube.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            YouTube Tweak: Open in FreeTube
+// @namespace       https://chrislowles.com/
+// @version         2026.5.18
+// @description     Fires the freetube:// protocol handler whenever a watch page loads.
+// @author          Chris Lowles, Claude
+// @license         AGPL-3.0-or-later
+// @match           http*://www.youtube.com/*
+// @match           http*://m.youtube.com/*
+// @updateURL       https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-in-freetube.user.js
+// @downloadURL     https://raw.githubusercontent.com/chrislowles/userscripts/main/yt-open-in-freetube.user.js
+// @run-at          document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  document.addEventListener('yt-navigate-finish', event => {
+    if (event.detail?.pageType === 'watch') {
+      window.open(`freetube://${window.location.href}`, '_top');
+    }
+  });
+
+})();
