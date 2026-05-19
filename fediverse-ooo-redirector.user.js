975d45103397e785ca0d0f03a9ee6494597e1d19
Chris Lowles <lowleschris@gmail.com>
2026-05-20 00:39:13 +1000

    Update fediverse-ooo-redirector.user.js

--- fediverse-ooo-redirector.user.js
+++ fediverse-ooo-redirector.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Fediverse > Local (fediverse.ooo) Redirector
 // @namespace    chrislowles.com
-// @version      2026.4.22
-// @description  Redirects posts on instances of fediverse software to fediverse.ooo.
+// @version      2026.5.20
+// @description  Redirects posts on instances of fediverse software to your local server through fediverse.ooo.
 // @author       Chris Lowles, Claude
 // @match        *://*/*
 // @exclude      *://fediverse.ooo/*
