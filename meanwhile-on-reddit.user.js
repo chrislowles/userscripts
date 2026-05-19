d615b22a036faf6596c44f302964d99568c5933c
Chris Lowles <lowleschris@gmail.com>
2026-05-20 00:39:18 +1000

    Update meanwhile-on-reddit.user.js

--- meanwhile-on-reddit.user.js
+++ meanwhile-on-reddit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Meanwhile on Reddit
 // @namespace     https://chrislowles.com/
-// @version       2026.5.12
+// @version       2026.5.20
 // @description   Injects a header bar on your Lemmy instance showing today's top 3 Reddit posts from a configurable set of subreddits. Also redirects Reddit URLs to your configured Redlib instance.
 // @author        Chris Lowles, Claude
 // @match         *://*/*
